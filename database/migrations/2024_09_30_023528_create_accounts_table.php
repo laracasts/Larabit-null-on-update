eaf1aadfe09b2bd2d5d762941c0c68dc3f6bfff6
Jeremy McPeak <jwmcpeak@gmail.com>
2024-10-02 23:13:37 -0500

    done

--- database/migrations/2024_09_30_023528_create_accounts_table.php
+++ database/migrations/2024_09_30_023528_create_accounts_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->foreignId('user_id')
                 ->nullable()
                 ->constrained()
-                ->nullOnDelete();
+                ->nullOnDelete()
+                ->nullOnUpdate();
 
             $table->string('name');
             $table->timestamps();
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,5 +7,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 it('account->user_id is null after user->id is changed', function () {
+    $manager = User::factory()
+        ->create([
+            'id' => 5001
+        ]);
 
+    $account = Account::factory()
+        ->recycle($manager)
+        ->create();
+
+    expect($manager->accounts->count())->toBe(1);
+
+    $manager->update([
+        'id' => 10001
+    ]);
+
+    $account->refresh();
+
+    expect($account->user_id)->toBeNull();
+
+    $manager2 = User::factory()
+        ->create([
+            'id' => 5100
+        ]);
+
+    $account->update([
+        'user_id' => $manager2->id,
+    ]);
+
+    expect($account->user_id)->toBe(5100);
 });
